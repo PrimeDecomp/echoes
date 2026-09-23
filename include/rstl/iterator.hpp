6e2074aa707c01e9ca46eb72f95177d3445c9650
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2026-09-23 19:10:19 +0300

    Match CPlayerState lower_bound helpers

--- include/rstl/iterator.hpp
+++ include/rstl/iterator.hpp
@@ -38,8 +38,8 @@ typename iterator_traits< It >::difference_type __distance(It first, It last,
 }
 
 template < typename It >
-typename iterator_traits< It >::difference_type __distance(It first, It last,
-                                                           random_access_iterator_tag) {
+inline typename iterator_traits< It >::difference_type __distance(It first, It last,
+                                                                  random_access_iterator_tag) {
   return last - first;
 }
 
@@ -69,12 +69,12 @@ void __advance(It& it, S count, bidirectional_iterator_tag) {
 }
 
 template < typename It, typename S >
-void __advance(It& it, S count, random_access_iterator_tag) {
+inline void __advance(It& it, S count, random_access_iterator_tag) {
   it += count;
 }
 
 template < typename It, typename S >
-void advance(It& it, S count) {
+inline void advance(It& it, S count) {
   __advance(it, count, typename iterator_traits< It >::iterator_category());
 }
 
