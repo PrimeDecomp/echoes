0d1f0e3b8fbaf3abc3eade3c558a747ad4d17900
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2026-09-25 02:24:10 +0300

    Match & link CMidiManager (#61)

--- configure.py
+++ configure.py
@@ -470,6 +470,7 @@ config.libs = [
             Object(MatchingFor("G2ME01"), "Kyoto/CTimeProvider.cpp"),
             Object(MatchingFor("G2ME01"), "Kyoto/CARAMToken.cpp"),
             Object(NonMatching, "Kyoto/CPakFile.cpp"),
+            Object(MatchingFor("G2ME01"), "Kyoto/Audio/CMidiManager.cpp"),
             Object(NonMatching, "Kyoto/Math/CMayaSpline.cpp"),
             Object(MatchingFor("G2ME01"), "Kyoto/Streams/CInputStream.cpp"),
             Object(MatchingFor("G2ME01"), "Kyoto/Streams/CBitStreamReader.cpp"),
--- include/Kyoto/Audio/CMidiManager.hpp
+++ include/Kyoto/Audio/CMidiManager.hpp
@@ -2,14 +2,58 @@
 #define _CMIDIMANAGER
 
 #include "Kyoto/Audio/CSfxHandle.hpp"
+#include "Kyoto/SObjectTag.hpp"
 
+#include "rstl/auto_ptr.hpp"
+#include "rstl/reserved_vector.hpp"
+
+class CInputStream;
 class CMidiManager {
 public:
-  class CMidiWrapper {};
-  class CMidiData {};
+  class CMidiWrapper {
+  public:
+    CMidiWrapper();
+    const CSfxHandle& GetManagerHandle() const;
+    const uint GetAudioSysHandle() const;
+    const bool IsAvailable() const;
 
-  static CSfxHandle Play(const CMidiData&, unsigned short fadeTime, bool stopExisting, short volume);
+    void SetAvailable(const bool v);
+    void SetAudioSysHandle(const uint handle);
+    const short GetSongId() const;
+    void SetMidiHandle(const CSfxHandle& handle);
+    void SetSongId(const short id);
+
+  private:
+    uint x0_sysHandle;
+    CSfxHandle x4_midiHandle;
+    short x8_songId;
+    bool xa_available;
+  };
+
+  class CMidiData {
+  public:
+    CMidiData(CInputStream& in);
+
+    const short GetSongId() const { return x0_songId; }
+    const short GetGroupId() const { return x2_groupId; }
+    CAssetId GetAGSCAssetId() const { return x4_agscId; }
+    uchar* GetData() const { return x8_data.get(); }
+
+  private:
+    short x0_songId;
+    short x2_groupId;
+    CAssetId x4_agscId;
+    rstl::auto_ptr< uchar > x8_data;
+  };
+
+  static CSfxHandle Play(const CMidiData&, unsigned short fadeTime, bool stopExisting,
+                         short volume);
   static void Stop(const CSfxHandle&, unsigned short);
+  static void StopAll();
+
+  static CSfxHandle LocateHandle();
+
+  static rstl::reserved_vector< CMidiWrapper, 3 > mMidiWrappers;
 };
 
 #endif // _CMIDIMANAGER
