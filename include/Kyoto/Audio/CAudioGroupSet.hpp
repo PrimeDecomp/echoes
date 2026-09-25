94bac7b9470bef9d18d8e525b9d275eec8f57c0b
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2026-09-25 18:14:08 +0300

    DolphinCAudioGroupSet 99%

--- include/Kyoto/Audio/CAudioGroupSet.hpp
+++ include/Kyoto/Audio/CAudioGroupSet.hpp
@@ -1,51 +1,35 @@
 #ifndef _CAUDIOGROUPSET
 #define _CAUDIOGROUPSET
 
-#include "Kyoto/TToken.hpp"
+#include "types.h"
 
 #include "rstl/auto_ptr.hpp"
-#include "rstl/optional_object.hpp"
 #include "rstl/single_ptr.hpp"
-#include "rstl/string.hpp"
 
-#include "Kyoto/CResFactory.hpp"
+#include "Kyoto/CFactoryMgr.hpp"
 
-class CInputStream;
+class CVParamTransfer;
+
+// Echoes AGSC: the group is pushed straight into ARAM through a MusyX sound stack.
 class CAudioGrpSetLoc {
 public:
   CAudioGrpSetLoc(const rstl::auto_ptr< uchar >& data, int length);
-  const rstl::string& GetBaseDirName() const { return mBaseDirName; }
-  const rstl::string& GetGroupSetName() const { return mGroupSetName; }
-  uint AramUsage() const;
-  const void* GetPool() const;
-  const void* GetProject() const;
-  const void* GetSample() const;
-  const void* GetSampleDir() const;
+  ~CAudioGrpSetLoc();
+
+  static bool sInSinglePlayer;
 
 private:
-  rstl::auto_ptr< uchar > mData;
-  rstl::auto_ptr< uchar > x8_;
-  rstl::string mBaseDirName;
-  rstl::string mGroupSetName;
-  uint x30_;
-  uint x34_;
-  uint x38_;
-  uint x3c_;
-  uint x40_;
+  uint mStackId;
+  const void* mAramAlloc;
+  rstl::single_ptr< uchar > mStackBuffer;
+  rstl::single_ptr< uchar > mPool;
+  rstl::single_ptr< uchar > mProject;
+  rstl::single_ptr< uchar > mSampleDir;
 };
+CHECK_SIZEOF(CAudioGrpSetLoc, 0x18)
 
-class CAudioGroupSet {
-public:
-  CAudioGroupSet(const TLockedToken< CAudioGrpSetLoc >& group);
-  ~CAudioGroupSet();
-  void Reload();
-  void FreeSampleBuffer();
-  void FreeAllBuffers();
-
-private:
-  rstl::string mBaseDir;
-  rstl::string mGroupSetName;
-  rstl::optional_object< TLockedToken< CAudioGrpSetLoc > > mGroupSetTok;
-};
+const CFactoryFnReturn FAudioGroupSetLocDataFactory(const SObjectTag& tag,
+                                                    const rstl::auto_ptr< uchar >& data, int length,
+                                                    const CVParamTransfer& xfer);
 
 #endif // _CAUDIOGROUPSET
