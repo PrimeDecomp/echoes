2be53de59e74463857b81d4bda333fd0f11b8501
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2026-09-25 11:27:07 +0300

    Use m-prefixed member names

--- src/Kyoto/Animation/CAdditiveAnimPlayback.cpp
+++ src/Kyoto/Animation/CAdditiveAnimPlayback.cpp
@@ -7,64 +7,64 @@
 CAdditiveAnimPlayback::CAdditiveAnimPlayback(const rstl::ncrc_ptr< CAnimTreeNode >& anim,
                                              float weight, bool loop,
                                              const CAdditiveAnimationInfo& info, bool fadeOut)
-: x0_info(info)
-, x8_anim(anim)
-, x10_targetWeight(rstl::max_val(0.f, rstl::min_val(weight, 1.f)))
-, x14_curWeight(0.f)
-, x18_active(loop)
-, x1c_weightTimer(0.f)
-, x20_phase(kPP_FadingIn)
-, x24_needsFadeOut(!loop && fadeOut) {}
+: mInfo(info)
+, mAnim(anim)
+, mTargetWeight(rstl::max_val(0.f, rstl::min_val(weight, 1.f)))
+, mCurWeight(0.f)
+, mActive(loop)
+, mWeightTimer(0.f)
+, mPhase(kPP_FadingIn)
+, mNeedsFadeOut(!loop && fadeOut) {}
 
 void CAdditiveAnimPlayback::SetWeight(float weight) {
-  x10_targetWeight = rstl::max_val(0.f, rstl::min_val(weight, 1.f));
+  mTargetWeight = rstl::max_val(0.f, rstl::min_val(weight, 1.f));
 
-  if (x20_phase == kPP_FadingIn) {
-    x14_curWeight = x0_info.GetFadeInTime() > 0.f
-                        ? x10_targetWeight * (x1c_weightTimer / x0_info.GetFadeInTime())
-                        : x10_targetWeight;
+  if (mPhase == kPP_FadingIn) {
+    mCurWeight = mInfo.GetFadeInTime() > 0.f
+                        ? mTargetWeight * (mWeightTimer / mInfo.GetFadeInTime())
+                        : mTargetWeight;
     return;
   }
-  if (x20_phase == kPP_FadingOut) {
-    x14_curWeight = x0_info.GetFadeOutTime() > 0.f
-                        ? x10_targetWeight * (x1c_weightTimer / x0_info.GetFadeOutTime())
-                        : x10_targetWeight;
+  if (mPhase == kPP_FadingOut) {
+    mCurWeight = mInfo.GetFadeOutTime() > 0.f
+                        ? mTargetWeight * (mWeightTimer / mInfo.GetFadeOutTime())
+                        : mTargetWeight;
     return;
   }
-  x14_curWeight = x10_targetWeight;
+  mCurWeight = mTargetWeight;
 }
 
 void CAdditiveAnimPlayback::FadeOut() {
-  if (x20_phase == kPP_FadedOut || x20_phase == kPP_FadedIn) {
-    x1c_weightTimer = x0_info.GetFadeOutTime();
-  } else if (x20_phase == kPP_FadingIn) {
-    x1c_weightTimer = (x1c_weightTimer / x0_info.GetFadeInTime()) * x0_info.GetFadeOutTime();
+  if (mPhase == kPP_FadedOut || mPhase == kPP_FadedIn) {
+    mWeightTimer = mInfo.GetFadeOutTime();
+  } else if (mPhase == kPP_FadingIn) {
+    mWeightTimer = (mWeightTimer / mInfo.GetFadeInTime()) * mInfo.GetFadeOutTime();
   }
 
-  if (x0_info.GetFadeOutTime() > 0.f) {
-    x20_phase = kPP_FadingOut;
+  if (mInfo.GetFadeOutTime() > 0.f) {
+    mPhase = kPP_FadingOut;
     return;
   }
 
-  x20_phase = kPP_FadedOut;
-  x14_curWeight = 0.f;
+  mPhase = kPP_FadedOut;
+  mCurWeight = 0.f;
 }
 
 void CAdditiveAnimPlayback::Update(float dt) {
-  if (x20_phase == kPP_FadingIn) {
-    const float time = x0_info.GetFadeInTime();
-    x1c_weightTimer = rstl::min_val(time, x1c_weightTimer + dt);
-    x14_curWeight = time > 0.f ? x10_targetWeight * (x1c_weightTimer / time) : x10_targetWeight;
+  if (mPhase == kPP_FadingIn) {
+    const float time = mInfo.GetFadeInTime();
+    mWeightTimer = rstl::min_val(time, mWeightTimer + dt);
+    mCurWeight = time > 0.f ? mTargetWeight * (mWeightTimer / time) : mTargetWeight;
 
-    if (close_enough(x14_curWeight, x10_targetWeight)) {
-      x20_phase = kPP_FadedIn;
+    if (close_enough(mCurWeight, mTargetWeight)) {
+      mPhase = kPP_FadedIn;
     }
-  } else if (x20_phase == kPP_FadingOut) {
-    const float time = x0_info.GetFadeOutTime();
-    x1c_weightTimer = rstl::max_val(0.f, x1c_weightTimer - dt);
-    x14_curWeight = time > 0.f ? x10_targetWeight * (x1c_weightTimer / time) : 0.f;
-    if (close_enough(x14_curWeight, 0.f)) {
-      x20_phase = kPP_FadedOut;
+  } else if (mPhase == kPP_FadingOut) {
+    const float time = mInfo.GetFadeOutTime();
+    mWeightTimer = rstl::max_val(0.f, mWeightTimer - dt);
+    mCurWeight = time > 0.f ? mTargetWeight * (mWeightTimer / time) : 0.f;
+    if (close_enough(mCurWeight, 0.f)) {
+      mPhase = kPP_FadedOut;
     }
   }
 }
