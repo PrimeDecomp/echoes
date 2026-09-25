f5e3568cd691a6e604e5e50c695777f953a45f7a
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2026-09-25 22:13:00 +0300

    Remove extra spacing between TypesMatch macros

--- src/MetroidPrime/TypesMatch.cpp
+++ src/MetroidPrime/TypesMatch.cpp
@@ -55,75 +55,41 @@ CEntity* CEntity::TypesMatch(int typeId) const {
 }
 
 TYPES_MATCH_IMPL(CActor, CEntity, kET_Actor)
-
 TYPES_MATCH_IMPL(CPhysicsActor, CActor, kET_PhysicsActor)
-
 TYPES_MATCH_IMPL(CScriptWaypoint, CActor, kET_ScriptWaypoint)
-
 TYPES_MATCH_IMPL(CScriptSequenceTimer, CEntity, kET_ScriptSequenceTimer)
-
 TYPES_MATCH_IMPL(CGameLight, CActor, kET_GameLight)
-
 TYPES_MATCH_IMPL(CPlayer, CPhysicsActor, kET_Player)
-
 TYPES_MATCH_IMPL(CScriptActor, CPhysicsActor, kET_ScriptActor)
-
 TYPES_MATCH_IMPL(CScriptCameraWaypoint, CScriptWaypoint, kET_ScriptCameraWaypoint)
-
 TYPES_MATCH_IMPL(CScriptEffect, CActor, kET_ScriptEffect)
-
 TYPES_MATCH_IMPL(CScriptPickup, CActor, kET_ScriptPickup)
-
 TYPES_MATCH_IMPL(CScriptPlatform, CPhysicsActor, kET_ScriptPlatform)
-
 TYPES_MATCH_IMPL(CScriptRepulsor, CActor, kET_ScriptRepulsor)
-
 TYPES_MATCH_IMPL(CScriptSound, CActor, kET_ScriptSound)
-
 TYPES_MATCH_IMPL(CScriptSpawnPoint, CEntity, kET_ScriptSpawnPoint)
-
 TYPES_MATCH_IMPL(CScriptStreamedMusic, CEntity, kET_ScriptStreamedMusic)
-
 TYPES_MATCH_IMPL(CScriptForgottenObject, CEntity, kET_ScriptForgottenObject)
 
 CAST_TO_REF_IMPL(CEntity, kET_Entity)
-
 CAST_TO_PTR_IMPL(CEntity, kET_Entity)
-
 CAST_TO_REF_IMPL(CScriptSequenceTimer, kET_ScriptSequenceTimer)
-
 CAST_TO_PTR_IMPL(CScriptSequenceTimer, kET_ScriptSequenceTimer)
-
 CAST_TO_PTR_IMPL(CGameLight, kET_GameLight)
-
 CAST_TO_REF_IMPL(CPlayer, kET_Player)
-
 CAST_TO_PTR_IMPL(CPlayer, kET_Player)
-
 CAST_TO_REF_IMPL(CScriptActor, kET_ScriptActor)
-
 CAST_TO_PTR_IMPL(CScriptActor, kET_ScriptActor)
-
 CAST_TO_PTR_IMPL(CScriptCameraWaypoint, kET_ScriptCameraWaypoint)
-
 CAST_TO_REF_IMPL(CScriptEffect, kET_ScriptEffect)
-
 CAST_TO_PTR_IMPL(CScriptEffect, kET_ScriptEffect)
-
 CAST_TO_REF_IMPL(CScriptPickup, kET_ScriptPickup)
-
 CAST_TO_PTR_IMPL(CScriptPickup, kET_ScriptPickup)
-
 CAST_TO_REF_IMPL(CScriptSpawnPoint, kET_ScriptSpawnPoint)
-
 CAST_TO_PTR_IMPL(CScriptSpawnPoint, kET_ScriptSpawnPoint)
-
 CAST_TO_REF_IMPL(CScriptStreamedMusic, kET_ScriptStreamedMusic)
-
 CAST_TO_PTR_IMPL(CScriptStreamedMusic, kET_ScriptStreamedMusic)
-
 CAST_TO_REF_IMPL(CScriptForgottenObject, kET_ScriptForgottenObject)
-
 CAST_TO_PTR_IMPL(CScriptForgottenObject, kET_ScriptForgottenObject)
 
 template <>
