fab95748a07218936c128400c763048501db1681
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2026-09-25 22:40:20 +0300

    Match & link CGenDescription

--- configure.py
+++ configure.py
@@ -513,6 +513,7 @@ config.libs = [
             Object(MatchingFor("G2ME01"), "Kyoto/CARAMToken.cpp"),
             Object(MatchingFor("G2ME01"), "Kyoto/Particles/CElectricDescription.cpp"),
             Object(MatchingFor("G2ME01"), "Kyoto/Particles/CSwooshDescription.cpp"),
+            Object(MatchingFor("G2ME01"), "Kyoto/Particles/CGenDescription.cpp"),
             Object(NonMatching, "Kyoto/CPakFile.cpp"),
             Object(MatchingFor("G2ME01"), "Kyoto/Audio/CMidiManager.cpp"),
             Object(MatchingFor("G2ME01"), "Kyoto/Text/CFontImageDef.cpp"),
--- include/Kyoto/Particles/CGenDescription.hpp
+++ include/Kyoto/Particles/CGenDescription.hpp
@@ -3,104 +3,115 @@
 
 #include "types.h"
 
+#include "Kyoto/CToken.hpp"
 #include "Kyoto/Particles/IElement.hpp"
 #include "Kyoto/TToken.hpp"
 
 #include "rstl/optional_object.hpp"
-#include "rstl/single_ptr.hpp"
 
-class CModel;
 class CElectricDescription;
-class CSwooshDescription;
+class CModel;
 class CSpawnSystemKeyframeData;
+class CSwooshDescription;
 
 class CGenDescription {
 public:
   typedef rstl::optional_object< TCachedToken< CModel > > TParticleModel;
   typedef rstl::optional_object< TCachedToken< CGenDescription > > TChildGeneratorDesc;
   typedef rstl::optional_object< TCachedToken< CSwooshDescription > > TSwooshGeneratorDesc;
-  typedef TCachedToken< CElectricDescription > TElectricGeneratorDesc;
+  typedef rstl::optional_object< CToken > TElectricGeneratorDesc;
 
   CGenDescription();
   ~CGenDescription();
 
-private:
-  rstl::single_ptr< CIntElement > mPSLT;
-  rstl::single_ptr< CIntElement > mPSWT;
-  rstl::single_ptr< CRealElement > mPSTS;
-  rstl::single_ptr< CVectorElement > mPOFS;
-  rstl::single_ptr< CIntElement > mSEED;
-  rstl::single_ptr< CRealElement > mLENG;
-  rstl::single_ptr< CRealElement > mWIDT;
-  rstl::single_ptr< CIntElement > mMAXP;
-  rstl::single_ptr< CRealElement > mGRTE;
-  rstl::single_ptr< CColorElement > mCOLR;
-  rstl::single_ptr< CIntElement > mLTME;
-  rstl::single_ptr< CEmitterElement > mEMTR;
-  bool mSORT : 1;
-  bool mMBLR : 1;
+  CIntElement* mPSLT;
+  CIntElement* mPSWT;
+  CRealElement* mPSTS;
+  CVectorElement* mPOFS;
+  CIntElement* mSEED;
+  CRealElement* mLENG;
+  CRealElement* mWIDT;
+  CIntElement* mMAXP;
+  CRealElement* mGRTE;
+  CColorElement* mCOLR;
+  CIntElement* mLTME;
+  CEmitterElement* mEMTR;
   bool mLINE : 1;
-  bool mLIT_ : 1;
+  bool mFXLL : 1;
   bool mAAPH : 1;
   bool mZBUF : 1;
-  bool mFXLL : 1;
-  bool mPMAB : 1;
-  bool mVMD4 : 1;
-  bool mVMD3 : 1;
-  bool mVMD2 : 1;
-  bool mVMD1 : 1;
-  bool mOPTS : 1;
-  bool mPMUS : 1;
-  bool mPMOO : 1;
-  bool mCIND : 1;
+  bool mSORT : 1;
+  bool mLIT_ : 1;
   bool mORNT : 1;
   bool mRSOP : 1;
-  rstl::single_ptr< CIntElement > mMBSP;
-  rstl::single_ptr< CRealElement > mSIZE;
-  rstl::single_ptr< CRealElement > mROTA;
-  rstl::single_ptr< CUVElement > mTEXR;
-  rstl::single_ptr< CUVElement > mTIND;
+  bool mMBLR : 1;
+  bool mPMAB : 1;
+  bool mPMUS : 1;
+  bool mPMOO : 1;
+  bool mVMD1 : 1;
+  bool mVMD2 : 1;
+  bool mVMD3 : 1;
+  bool mVMD4 : 1;
+  bool mCIND : 1;
+  bool mINDM : 1;
+  bool mOPTS : 1;
+  bool mRDOP : 1;
+  bool mVMPC : 1;
+  CIntElement* mMBSP;
+  CRealElement* mSIZE;
+  CRealElement* mROTA;
+  CUVElement* mTEXR;
+  CUVElement* mTIND;
+  CIntElement* mXTAD;
+  CRealElement* mFXBR;
+  CVectorElement* mFXBO;
   TParticleModel mPMDL;
-  rstl::single_ptr< CVectorElement > mPMOP;
-  rstl::single_ptr< CVectorElement > mPMRT;
-  rstl::single_ptr< CVectorElement > mPMSC;
-  rstl::single_ptr< CColorElement > mPMCL;
-  rstl::single_ptr< CModVectorElement > mVEL1;
-  rstl::single_ptr< CModVectorElement > mVEL2;
-  rstl::single_ptr< CModVectorElement > mVEL3;
-  rstl::single_ptr< CModVectorElement > mVEL4;
+  CVectorElement* mPMOP;
+  CVectorElement* mPMRT;
+  CVectorElement* mPMSC;
+  CColorElement* mPMCL;
+  CVectorElement* mPMOV;
+  CModVectorElement* mVEL1;
+  CModVectorElement* mVEL2;
+  CModVectorElement* mVEL3;
+  CModVectorElement* mVEL4;
   TChildGeneratorDesc mICTS;
-  rstl::single_ptr< CIntElement > mNCSY;
-  rstl::single_ptr< CIntElement > mCSSD;
+  CIntElement* mNCSY;
+  CIntElement* mCSSD;
   TChildGeneratorDesc mIDTS;
-  rstl::single_ptr< CIntElement > mNDSY;
+  CIntElement* mNDSY;
   TChildGeneratorDesc mIITS;
-  rstl::single_ptr< CIntElement > mPISY;
-  rstl::single_ptr< CIntElement > mSISY;
-  rstl::single_ptr< CSpawnSystemKeyframeData > mKSSM;
+  CIntElement* mPISY;
+  CIntElement* mSISY;
+  CSpawnSystemKeyframeData* mKSSM;
   TSwooshGeneratorDesc mSSWH;
-  rstl::single_ptr< CIntElement > mSSSD;
-  rstl::single_ptr< CVectorElement > mSSPO;
+  CIntElement* mSSSD;
+  CVectorElement* mSSPO;
   TElectricGeneratorDesc mSELC;
-  rstl::single_ptr< CIntElement > mSESD;
-  rstl::single_ptr< CVectorElement > mSEPO;
-  rstl::single_ptr< CIntElement > mLTYP;
-  rstl::single_ptr< CColorElement > mLCLR;
-  rstl::single_ptr< CRealElement > mLINT;
-  rstl::single_ptr< CVectorElement > mLOFF;
-  rstl::single_ptr< CVectorElement > mLDIR;
-  rstl::single_ptr< CIntElement > mLFOT;
-  rstl::single_ptr< CRealElement > mLFOR;
-  rstl::single_ptr< CRealElement > mLSLA;
-  rstl::single_ptr< CRealElement > mADV1;
-  rstl::single_ptr< CRealElement > mADV2;
-  rstl::single_ptr< CRealElement > mADV3;
-  rstl::single_ptr< CRealElement > mADV4;
-  rstl::single_ptr< CRealElement > mADV5;
-  rstl::single_ptr< CRealElement > mADV6;
-  rstl::single_ptr< CRealElement > mADV7;
-  rstl::single_ptr< CRealElement > mADV8;
+  CIntElement* mSESD;
+  CVectorElement* mSEPO;
+  CIntElement* mLTYP;
+  CColorElement* mLCLR;
+  CRealElement* mLINT;
+  CVectorElement* mLOFF;
+  CVectorElement* mLDIR;
+  CIntElement* mLFOT;
+  CRealElement* mLFOR;
+  CRealElement* mLSLA;
+  CRealElement* mADV1;
+  CRealElement* mADV2;
+  CRealElement* mADV3;
+  CRealElement* mADV4;
+  CRealElement* mADV5;
+  CRealElement* mADV6;
+  CRealElement* mADV7;
+  CRealElement* mADV8;
+  CRealElement* mADV9;
+  CVectorElement* mVAV1;
+  CVectorElement* mVAV2;
+  CVectorElement* mVAV3;
+  int mDFLG;
 };
-CHECK_SIZEOF(CGenDescription, 0x12c)
+CHECK_SIZEOF(CGenDescription, 0x150)
 
 #endif // _CGENDESCRIPTION
