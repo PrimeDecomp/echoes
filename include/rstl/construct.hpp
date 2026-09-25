b43cd4ff3da2f31f45a061b8a16e2c054c12eeea
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2026-09-25 19:11:04 +0300

    Add rstl::construct<uint> specialization

--- include/rstl/construct.hpp
+++ include/rstl/construct.hpp
@@ -71,6 +71,11 @@ inline void construct< int >(void* dest, const int& src) {
   *static_cast< int* >(dest) = src;
 }
 
+template <>
+inline void construct< uint >(void* dest, const uint& src) {
+  *static_cast< uint* >(dest) = src;
+}
+
 template < typename T >
 static inline void destroy_impl(T* in) {
   in->~T();
